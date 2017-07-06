76080dd11757a3d367545aa9aeb509121681e02e
Unknown <acoleman@evanstonwe.org>
2017-07-06 11:38:22 -0500

    Teleportation

---
+++ Assets/BadTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadTrigger : MonoBehaviour {
+	Vector3 startPos = new Vector3 (-30,2,20);
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+		transform.position = startPos;
+	}
+}
