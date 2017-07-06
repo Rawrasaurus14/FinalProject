bda0834860bd29d0f3b424f45109b3b15fcf6ef7
Unknown <acoleman@evanstonwe.org>
2017-07-06 13:52:02 -0500

    FINAL PLACE TELEPORTATIONNNANANAANNA

---
+++ Assets/GoodTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodTrigger : MonoBehaviour {
+	Vector3 startPos = new Vector3 (-657,2,-584);
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+		transform.position = startPos;
+}
+}
